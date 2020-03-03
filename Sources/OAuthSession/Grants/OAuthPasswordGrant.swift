adefedd0e56ea6b209774746904546d671a75007
Jacob Christie <19879272+jjatie@users.noreply.github.com>
2020-03-03 10:50:10 -0400

    Add Audience to password grant

--- Sources/OAuthSession/Grants/OAuthPasswordGrant.swift
+++ Sources/OAuthSession/Grants/OAuthPasswordGrant.swift
@@ -9,17 +9,30 @@ public struct OAuthPasswordGrant: OAuthGrant {
     /// <#Description#>
     public let scope: Set<OAuthScope>
 
+    public let audience: String?
+
     public var parameters: [String : String] {
         let scope = self.scope.reduce(into: self.scope.first?.rawValue) {
             $0?.append(", \($1.rawValue)")
         }
 
-        return [
-            "username" : username,
-            "password" : password,
-            "grant_type" : "password",
-            "scope" : scope ?? ""
-        ]
+        return {
+            var dict = [
+                "username" : username,
+                "password" : password,
+                "grant_type" : "password",
+            ]
+            
+            if let scope = scope {
+                dict["scope"] = scope
+            }
+
+            if let audience = audience {
+                dict["audience"] = audience
+            }
+
+            return dict
+        }()
     }
     
     /// <#Description#>
@@ -27,10 +40,17 @@ public struct OAuthPasswordGrant: OAuthGrant {
     ///   - username: <#username description#>
     ///   - password: <#password description#>
     ///   - scope: <#scope description#>
-    public init(username: String, password: String, scope: Set<OAuthScope>) {
+    ///   - audience: <#audience description#>
+    public init(
+        username: String,
+        password: String,
+        scope: Set<OAuthScope>,
+        audience: String? = nil
+    ) {
         self.username = username
         self.password = password
         self.scope = scope
+        self.audience = audience
     }
 }
 
